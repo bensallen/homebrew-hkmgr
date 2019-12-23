61c2c3eabe5e527e566e2db5571a6c6975caf647
goreleaserbot <goreleaser@carlosbecker.com>
2019-12-23 11:20:40 -0600

    Brew formula update for hkmgr version 0.0.7

--- hkmgr.rb
+++ hkmgr.rb
@@ -2,12 +2,12 @@
 class Hkmgr < Formula
   desc "Hkmgr is a virtual machine manager for the hyperkit macOS hypervisor."
   homepage "https://github.com/bensallen/hkmgr"
-  version "0.0.6"
+  version "0.0.7"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/bensallen/hkmgr/releases/download/v0.0.6/hkmgr_0.0.6_Darwin_x86_64.tar.gz"
-    sha256 "ea4f186e5da7c554e9b42e7bbff108db1c85099213b134e41c1211f3d327c96d"
+    url "https://github.com/bensallen/hkmgr/releases/download/0.0.7/hkmgr_0.0.7_Darwin_x86_64.tar.gz"
+    sha256 "12ad33cf4ceb455d2808eecd2181ea1e447f91fefa4a7ab05c42092912c73daf"
   elsif OS.linux?
   end
   
